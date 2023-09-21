6bbb7d7091686ee917d41bc5a9ac50eef538571b
Meghana Reddy <meghana@Meghanas-MBP-3.fios-router.home>
2023-09-21 17:46:56 -0400

    made changes

--- main.go
+++ main.go
@@ -14,3 +14,11 @@ func main() {
 		log.Fatal(i18n.AppCrashed(err))
 	}
 }
+
+// 1.This is where our CLI application begins. It follows the standard structure for Go code.
+// 2.The main function starts and guides our command-line application.
+// 3.It imports packages like fmt, log, and custom packages from the github.com/taubyte/tau repository.
+//   a.fmt helps us with formatting and printing.
+//   b.log keeps a record of important events.
+//   c.We also have some custom packages from the github.com/taubyte/tau repository that add specific functionalities.
+// 4.It generates a new instance of TauCliApp from the commands package and proceeds to execute it, supplying the command-line arguments as input.
